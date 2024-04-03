6ca11c0753481c309b5192723d4577314e954269
vineth2002 <v18nallaperuma@gmail.com>
2024-04-03 19:08:10 +0530

    New Update

---
+++ Backend/index.js
@@ -0,0 +1,33 @@
+const http = require('http');
+
+
+
+const hostname = '127.0.0.1';
+
+const port = 3000;
+
+
+
+const server = http.createServer((req, res) => {
+
+    res.statusCode = 200;
+
+    
+
+    // Set CORS headers
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.end('Pizza\n');
+
+});
+
+
+
+server.listen(port, hostname, () => {
+
+    console.log(`Server running at http://${hostname}:${port}/`);
+
+});
